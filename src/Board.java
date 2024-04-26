e3ec8cc43c79a6e7611bc958c16b34ac333e9cf6
Evan Mitchell <ejm0214@gmail.com>
2024-04-26 19:06:40 -0400

    Working on board and cell communication

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,37 @@
 public class Board {
+    /**
+     * Global matrix for storing board contents
+     */
+    private int[][] board;
+
+    /**
+     * Constructs Board based on input number of rows and columns
+     * @param rows Number of rows in Board
+     * @param cols Number of columns in Board
+     */
+    public Board(int rows, int cols) {
+        board = new int[rows][cols];
+    }
+
+    /**
+     * Displays current board in terminal
+     */
+    public void display() {
+        for(int i = 0; i < board.length; i++) {
+            System.out.print("| ");
+            for(int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+    }
+
+    /**
+     * Adds a cell to the board at the coordinates of the cell
+     * @param x Cell's X coordinate
+     * @param y Cell's Y coordinate
+     */
+    public void add(int x, int y) {
+        board[x][y] = 1;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        Board board = new Board(10, 10);
+        board.display();
     }
 }
