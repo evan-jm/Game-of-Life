3a7d309f7758a518a9744d2b1d51b42c096b6d27
Evan Mitchell <ejm0214@gmail.com>
2024-04-28 18:35:44 -0400

    Fix

---
+++ src/display/Panel.java
@@ -0,0 +1,13 @@
+package display;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+
+/**
+ * Contents portrayed on frame
+ */
+public class Panel extends JPanel {
+    public Panel(int rows, int cols) {
+        GridLayout gridLayout = new GridLayout(rows, cols);
+        this.setLayout(gridLayout);
+    }
+}
