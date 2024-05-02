179dd5cba6d610c6643fd75cd12df6856267a78c
evan-jm <ejm0214@gmail.com>
2024-05-01 20:19:39 -0400

    Adding main

---
+++ app/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+import org.example.main.Board;
+
+public class Main {
+    public static void main(String[] args) {
+        int rows = 10;
+        int cols = 10;
+        Board board = new Board(rows, cols);
+        board.addRandomCells(50);
+        while(true) {
+            try {
+                board.display();
+                Thread.sleep(1000);
+                board.run();
+                System.out.println();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
